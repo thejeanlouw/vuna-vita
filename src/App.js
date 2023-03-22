1f98ded39dab599bebeace1897e2a94a6fe4b531
Jean Louw <thejeanlouw@gmail.com>
2023-03-22 14:23:22 +0200

    Create the page

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import SymptomInputForm from './components/symptom-input-form/symptom-input-form.component';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <SymptomInputForm />
+    </div>
+  );
+}
+
+export default App;
